f15685bf1b382fb842c16c6c899208a4244c8ac4
renebier <rene.bierbaumer04@gmail.com>
2026-07-20 09:40:46 +0200

    teams integration working

--- src/db_connector.py
+++ src/db_connector.py
@@ -53,23 +53,15 @@ class DatabaseConnector(object):
             print("Failed to connect to the database.")
             return False
         
-    query = """
-SELECT act.Oid, act.createdOn, act.Subject, act.Duration, proj.Name AS ProjectName, cli.Name AS ClientName, usr.UserName AS UserName, actTy.Name AS ActivityTypeName
-FROM dbo.Activity act 
-LEFT JOIN dbo.Client cli ON act.Client = cli.Oid 
-LEFT JOIN dbo.[User] usr ON act.CreatedBy = usr.Oid 
-LEFT JOIN dbo.ActivityType actTy ON act.ActivityType = actTy.Oid
-LEFT JOIN dbo.Project proj ON act.Project = proj.Oid
-WHERE
-not actTy.Name = 'Urlaub' ORDER BY act.CreatedOn, act.Duration DESC;
-"""
-    def execute_query(self, query = self.query):
+
+    def execute_query(self, query = "SELECT 1;"):
         """
         Executes a SQL query and returns the results.
         """
         if self.connection:
             cursor = self.connection.cursor()
             cursor.execute(query)
+            print(f"Executed query: {query}")
             columns = [desc[0] for desc in cursor.description]
             results = cursor.fetchall()
             df = pd.DataFrame(results, columns=columns)
--- src/main.py
+++ src/main.py
@@ -5,7 +5,16 @@ import pandas as pd
 from llm_interface import LLMInterface
 from teams_handler import TeamsConnector
 
-
+QUERY = """
+SELECT act.Oid, act.createdOn, act.Subject, act.Duration, proj.Name AS ProjectName, cli.Name AS ClientName, usr.UserName AS UserName, actTy.Name AS ActivityTypeName
+FROM dbo.Activity act 
+LEFT JOIN dbo.Client cli ON act.Client = cli.Oid 
+LEFT JOIN dbo.[User] usr ON act.CreatedBy = usr.Oid 
+LEFT JOIN dbo.ActivityType actTy ON act.ActivityType = actTy.Oid
+LEFT JOIN dbo.Project proj ON act.Project = proj.Oid
+WHERE
+not actTy.Name = 'Urlaub' ORDER BY act.CreatedOn, act.Duration DESC;
+"""
 
 
 
@@ -13,7 +22,7 @@ if __name__ == "__main__":
     load_dotenv()
     try:
         with DatabaseConnector() as db:
-            res = db.execute_query()
+            res = db.execute_query(query=QUERY)
     except Exception as e:
         print(f"Error occurred: {e}")
     res["Duration"] = res["Duration"].apply(lambda x: x / 3600 if pd.notnull(x) else 0)
